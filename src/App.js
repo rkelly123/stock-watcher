0006857554706073fc5c0f45ffc18b5c5fecd52c
rkelly03 <rkelly9812@gmail.com>
2021-12-30 04:32:04 -0500

    User Registration And Login
    User Registration and Login now set up using Auth0.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Stock from './Stock';
 import './App.css';
+import LoginButton from './user_registration_components/LoginButton'
+import LogoutButton from './user_registration_components/LogoutButton'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isLoading } = useAuth0();
+  
+  if (isLoading) return <div>Loading ...</div>
+
   return (
     <div className="App">
+      <div>
+        <LoginButton />
+        <LogoutButton />
+      </div>
       <Stock></Stock>
     </div>
   );
